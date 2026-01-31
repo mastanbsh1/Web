0e5805dbb7b5a827d1aacf4ef51cd4ac92cbff03
thiyagarajan.m <thiyagarajan.m@travelodge.co.uk>
2026-01-31 09:02:32 +0000

    Smoke tests included

---
+++ infra/terraform/test/jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: 'node',
+  testMatch: ['**/Web/infra/terraform/test/**/*.go'],
+  // This is a placeholder for Go tests; actual Go tests should be run with `go test`
+};
